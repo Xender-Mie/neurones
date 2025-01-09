0d9c5ba7eaa8d43a21d996193d0818f2a7769b15
Xender <109360639+Xender-Mie@users.noreply.github.com>
2025-01-09 21:30:10 +0100

    English translation

---
+++ layer_neurons.h
@@ -0,0 +1,30 @@
+#ifndef LAYER_NEURONS_H
+#define LAYER_NEURONS_H
+#include "list.h"
+#include "neuron.h"
+
+typedef struct elemLayer {
+    neuron neur;
+    struct elemLayer* next;
+} elementLayer;
+
+typedef elementLayer* LayerNeurons;
+
+/**
+ * Function that creates a layer and initializes its neurons.
+ * @param NbNeurons Number of neurons present in the layer.
+ * @param NbEntries Number of neurons input.
+ * @return
+ */
+LayerNeurons InitCouche(int NbNeurons, int NbEntries);
+
+/**
+ * Function that allows to calculate the list of outputs of a neurone’s skin.
+ * @param layer The layer for which outputs are to be calculated.
+ * @param entries List of entries of the layer.
+ * @param NbEntries Number of entries of the layer.
+ * @return
+ */
+list Outcouche(LayerNeurons layer, list entries, int NbEntries);
+
+#endif //LAYER_NEURONS_H
